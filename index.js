fe701349a7a99aa147438731b0f5b6086c6b0955
milhamap <ilhamap45@gmail.com>
2023-01-07 17:41:12 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+
+const app = express();
+
+// production
+app.use(cookieParser());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port http://localhost:${process.env.PORT}`);
+})
+
+module.exports = app;
---
+++ src/database/index.js
@@ -0,0 +1,22 @@
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '.env') });
+
+const knex = require('knex')({
+    client: process.env.DB_CLIENT,
+    connection: {
+        server: process.env.DB_SERVER,
+        database: process.env.DB_DATABASE,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        options: {
+            encrypt: true,
+            enableArithAbort: true,
+            multipleActiveResultSets: false,
+            trustServerCertificate: false,
+            connectionTimeout: 30,
+        },
+        port: 1433
+    }
+})
+
+module.exports = knex;
